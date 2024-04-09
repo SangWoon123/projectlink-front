87af1b208ff6f7998ecbc27b07324567de617a15
songhaechan <bukak2019@naver.com>
2024-04-09 13:14:08 +0900

    feature:로그인,회원가입,소셜로그인 페이지 구현

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-    <router-link to="/board">Board컴포넌트이동</router-link>
-    <router-link to="/comment">Comment컴포넌트이동</router-link>
-    <router-view />
+  <the-navigation></the-navigation>
+  <router-link to="/board">Board컴포넌트이동</router-link>
+  <router-link to="/comment">Comment컴포넌트이동</router-link>
+  <router-view />
 </template>
 
 <script>
+import TheNavigation from "./components/base/TheNavigation.vue";
 // import BoardPage from './components/board/BoardPage.vue';
 // import CommentComponent from './components/comment/CommentComponent.vue';
 
 export default {
-    name: 'App',
-    // components: {
-    //     BoardPage, CommentComponent
-    // },
+  components: { TheNavigation },
+  name: "App",
+  // components: {
+  //     BoardPage, CommentComponent
+  // },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/base/TheNavigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <nav class="navbar">
+      <div class="navbar-logo">
+        <img src="../../assets/logo.png" width="60px" />
+      </div>
+      <div class="navbar-menu">
+        <ul>
+          <li>
+            <router-link to="/login">로그인</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.navbar-logo {
+  padding-top: 10px;
+  padding-left: 10px;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  padding-left: 0;
+  padding-right: 20px;
+}
+</style>
